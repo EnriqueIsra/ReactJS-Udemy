86391d51e874ebea569536b5c50783f8bfadf8b4
EnriqueIsra <sirilo99x3@gmail.com>
2025-08-07 13:45:28 -0600

    Ejercicio de codificacion 1: Excercise: Working with Functions

---
+++ 00-starting-js-proj/assets/scripts/excercises.js
@@ -0,0 +1,19 @@
+// Exercise: Working with Functions
+
+// Your task is to write a new function that should be named combine and have the following characteristics:
+
+//     Accept three input values
+
+//     Calculate a new value based on the three input values: a * b / c (if a, b & c are the input values)
+
+//     Return the calculated result
+
+// Create a new function that utilizes parameters and returns a value.
+
+function combine(a, b, c) {
+    return ((a * b) / c)
+}
+const result1 = combine(2, 2, 2)
+console.log(result1)
+const result2 = combine(4, 5, 2)
+console.log(result2)
