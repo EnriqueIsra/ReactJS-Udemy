07c45a73f27263ac25805553daa2e5d3ccde6de1
EnriqueIsra <sirilo99x3@gmail.com>
2025-08-07 11:33:47 -0600

    18. Revisiting Operators

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -1,8 +1,14 @@
 // import { apyKey } from "./util.js";
 
 // import apiKey from "./util.js"
-import { apiKey, abc as content } from "./util.js";
+// import { apiKey, abc as content } from "./util.js";
 // import * as util from "./util.js";
 
 // console.log(util.default);
-console.log(content);
+// console.log(content);
+
+// let userMessage = "Hello World"
+const userMessage = "Hello World"
+
+console.log(userMessage);
+console.log(userMessage);
