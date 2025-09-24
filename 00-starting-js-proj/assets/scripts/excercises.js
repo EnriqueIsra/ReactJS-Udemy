9e3c0816464f8322aa97b968d593d0f4c0cb5bd2
EnriqueIsra <sirilo99x3@gmail.com>
2025-09-23 23:42:21 -0600

    22. Revisiting Object & Classes

--- 00-starting-js-proj/assets/scripts/excercises.js
+++ 00-starting-js-proj/assets/scripts/excercises.js
@@ -10,10 +10,10 @@
 
 // Create a new function that utilizes parameters and returns a value.
 
-function combine(a, b, c) {
-    return ((a * b) / c)
-}
-const result1 = combine(2, 2, 2)
-console.log(result1)
-const result2 = combine(4, 5, 2)
-console.log(result2)
+// function combine(a, b, c) {
+//     return ((a * b) / c)
+// }
+// const result1 = combine(2, 2, 2)
+// console.log(result1)
+// const result2 = combine(4, 5, 2)
+// console.log(result2)
