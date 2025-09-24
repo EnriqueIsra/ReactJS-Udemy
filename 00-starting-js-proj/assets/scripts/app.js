f01f30d721fe5ef1837c629d0815c69e418e787e
EnriqueIsra <sirilo99x3@gmail.com>
2025-09-24 11:11:51 -0600

    28. Manipulating the DOM - Not With React

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -95,17 +95,20 @@
 // }
 // console.log(extendedUser)
 
-const password = prompt("Your password")
+// const password = prompt("Your password")
 
-if(password === "Hello"){
-    console.log("Hello works")
-} else if (password === "hello"){
-    console.log("hello works")
-} else {
-    console.log("Access not granted")
-}
+// if(password === "Hello"){
+//     console.log("Hello works")
+// } else if (password === "hello"){
+//     console.log("hello works")
+// } else {
+//     console.log("Access not granted")
+// }
 
-const hobbies = ["Sports", "Cooking"]
-for (const hobby of hobbies) {
-    console.log(hobby)
-}
+// const hobbies = ["Sports", "Cooking"]
+// for (const hobby of hobbies) {
+//     console.log(hobby)
+// }
+
+const list = document.querySelector("ul")
+list.remove();
